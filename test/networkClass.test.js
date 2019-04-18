c5b0a7e410ae990d92e107c1dea5b24925ed16c6
Elena <litishia@ya.ru>
2019-04-18 17:10:59 -0400

    add tests for getNetworkMethod for both versions

--- test/networkClass.test.js
+++ test/networkClass.test.js
@@ -143,6 +143,19 @@ describe('Valid, test networkToInteger method', () => {
     });
 });
 
+describe('Valid, testing getNetwork method', () => {
+    test.each`
+    address              |prefix   | expected
+    ${'192.168.98.2'}    | ${6}    | ${'192'}
+    ${'255.168.114.128'} | ${17}   | ${'255.168'}
+    ${'1:dead::987'}     | ${99}   | ${'1:dead::'}
+    ${'2:be::b3:0:2:3'}  | ${70}   | ${'2:be::'}
+    `('returns $expected network for ip $address',({address, prefix, expected}) => {
+    const net = new Network(address, prefix);
+    expect(net.getNetwork()).toBe(expected);
+});
+});
+
 describe('IPv6 test ALL network methods for FE80:0000:0000:0000:0202:B3FF:FE1E:8329', () => {
     const net = new Network('FE80:0000:0000:0000:0202:B3FF:FE1E:8329', 42);
 
